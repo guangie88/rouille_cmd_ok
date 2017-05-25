ad9b79ef8dd9783685282f0e5753fe353afd0a19
Chen Weiguang <chen.weiguang@gmail.com>
2017-05-25 20:47:08 +0800

    Initial commit rouille implementation that works okay with std::process::Command on Windows 7.

---
+++ README.md
@@ -0,0 +1,2 @@
+# rouille_cmd_ok
+Demonstrate that is okay with rouille and std::process::Command for Windows 7.
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+#[macro_use]
+extern crate rouille;
+extern crate hyper;
+
+use hyper::client::{Client, Response};
+use std::time::Duration;
+use std::io::Read;
+use std::process::{Command, Output};
+use std::thread;
+
+fn exec_cmd(cmd: &str) -> Output {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd").args(&["/C", cmd]).output()
+    } else {
+        Command::new("sh").args(&["-c", cmd]).output()
+    }.unwrap()
+}
+
+fn read_body(rsp: &mut Response) -> String {
+    let mut s = String::new();
+    rsp.read_to_string(&mut s).unwrap();
+    s
+}
+
+fn execute() -> String {
+    const CMD: &str = "echo hello";
+    format!("{:?}", exec_cmd(CMD))
+}
+
+fn main() {
+    thread::spawn(|| {
+        const WAIT_MS: u64 = 1000;
+        const LOOP_COUNT: usize = 50;
+
+        println!("Client sleeping for {} ms before starting...", WAIT_MS);
+        thread::sleep(Duration::from_millis(WAIT_MS));
+
+        let cmd_map = (0..LOOP_COUNT)
+            .map(|index| {
+                let client = Client::new();
+                let mut rsp = client.get("http://localhost:8000/").send().unwrap();
+                (index, read_body(&mut rsp))
+            });
+
+        for (index, body) in cmd_map {
+            println!("#{:02}: {}", index, body);
+        }
+
+        println!("Client completed! Press CTRL-C to exit...");
+    });
+
+    rouille::start_server("localhost:8000", move |request| {
+        router!(request,
+            (GET) (/) => { rouille::Response::text(execute()) },
+            _ => { rouille::Response::empty_400() }
+        )
+    });
+}
